6cd187d6fda3e0db4e727b76a3f310612570d2d7
TomoDevelops WSL <wsl@tomodevelops.com>
2024-09-06 17:40:51 +0900

    /login

---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>Login</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { sessionCookie } = useAuth();
+
+const onSignIn = async () => {
+  sessionCookie.value = null;
+};
+</script>
