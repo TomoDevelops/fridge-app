12d7985ee116dcae4d4a5b84db1a4349b9cd353c
TomoDevelops WSL <wsl@tomodevelops.com>
2024-09-06 17:40:08 +0900

    Loader

---
+++ components/shared/Loading.vue
@@ -0,0 +1,13 @@
+<template>
+  <div className="flex justify-center space-x-5 rounded-full p-5 duration-75">
+    <div
+      className="h-8 w-8 animate-bounce rounded-full bg-[#7148fc] delay-100"
+    ></div>
+    <div
+      className="h-8 w-8 animate-bounce rounded-full bg-[#05C3DE] delay-300"
+    ></div>
+    <div
+      className="h-8 w-8 animate-bounce rounded-full bg-[#94a3b8] delay-500"
+    ></div>
+  </div>
+</template>
