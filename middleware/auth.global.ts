debbf07a7bd5691ae9e8d0e67201226c163a349a
TomoDevelops WSL <wsl@tomodevelops.com>
2024-09-06 22:20:44 +0900

    middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,8 +1,15 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
   const sessionCookie = useCookie("session");
+
   if (!sessionCookie.value) {
-    if (to.path !== "/login") {
-      return navigateTo("/login");
+    if (to.path !== "/signin") {
+      return navigateTo("/signin");
+    }
+  }
+
+  if (sessionCookie.value) {
+    if (to.path === "/signin") {
+      return navigateTo("/");
     }
   }
 });
